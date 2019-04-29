7571473dc9b2c579f07e108d0015942dc343ede5
Jiachen-Dell <acezjc@foxmail.com>
2019-04-30 00:33:13 +0800

    finish decoder

---
+++ src/project_10/instruction_decoder/instruction_decoder.sim/idcode32_sim.v
@@ -0,0 +1,95 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2016/08/07 10:27:17
+// Design Name: 
+// Module Name: idcode32_sim
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module idcode32_sim( );
+    // input 
+    reg[31:0]  Instruction = 32'b000000_00010_00011_00111_00000_100000; //add $7,$2,$3
+    reg[31:0]  read_data = 32'h00000000;                    //  从DATA RAM or I/O port取出的数据
+    reg[31:0]  ALU_result = 32'h00000005;                   //  需要扩展立即数到32位
+    reg        Jal = 1'b0; 
+    reg        RegWrite = 1'b1;
+    reg        MemtoReg = 1'b0;
+    reg        RegDst = 1'b1;
+    reg        clock = 1'b0 ,reset = 1'b1;
+    reg[31:0]  opcplus4 = 32'h00000004;                 // 来自取指单元，JAL中用
+    // output
+    wire[31:0] read_data_1;
+    wire[31:0] read_data_2;
+    wire[31:0] Sign_extend;
+    reg[31:0] register[0:31];
+    
+    decoder Uid(.read_data_1(read_data_1),.read_data_2(read_data_2),.Instruction(Instruction),.read_data(read_data),.ALU_result(ALU_result),
+                     .Jal(Jal),.RegWrite(RegWrite),.MemtoReg(MemtoReg),.RegDst(RegDst),.Sign_extend(Sign_extend),.clk(clock),.rst(reset),
+                     .opcplus4(opcplus4), .register(register));
+
+    initial begin
+        #20   reset = 1'b0;
+        #20   begin Instruction = 32'b001000_00111_00011_1000000000110111;  //addi $3,$7,0X8037
+                    read_data = 32'h00000000; 
+                    ALU_result = 32'hFFFF803C;
+                    Jal = 1'b0;
+                    RegWrite = 1'b1;
+                    MemtoReg = 1'b0;
+                    RegDst = 1'b0;
+                    opcplus4 = 32'h00000008; 
+               end
+            //begin Instruction = 32'b001100_00010_00100_1000000010010111;  //andi $4,$2,0X8097
+        #20   begin Instruction = 32'b001100_00010_00100_1000000010010111;  //andi $4,$2,0X8097 
+                           read_data = 32'h00000000; 
+                           ALU_result = 32'h00000002;
+                           Jal = 1'b0;
+                           RegWrite = 1'b1;
+                           MemtoReg = 1'b0;
+                           RegDst = 1'b0;
+                           opcplus4 = 32'h0000000c; 
+                end
+        #20   begin Instruction = 32'b000000_00000_00001_00101_00010_000000;  //sll $5,$1,2
+                                   read_data = 32'h00000000; 
+                                   ALU_result = 32'h00000004;
+                                   Jal = 1'b0;
+                                   RegWrite = 1'b1;
+                                   MemtoReg = 1'b0;
+                                   RegDst = 1'b1;
+                                   opcplus4 = 32'h00000010; 
+               end
+        #20   begin Instruction = 32'b100011_00000_00110_0000000100000000;  //LW $6,0(0X100)
+                                          read_data = 32'h0000007B; 
+                                          ALU_result = 32'h00000054;
+                                          Jal = 1'b0;
+                                          RegWrite = 1'b1;
+                                          MemtoReg = 1'b1;
+                                          RegDst = 1'b0;
+                                          opcplus4 = 32'h00000014; 
+               end
+        #20   begin Instruction = 32'b000011_00000000000000000000000000;  //JAL 0000
+                                          read_data = 32'h00000000; 
+                                          ALU_result = 32'h00000004;
+                                          Jal = 1'b1;
+                                          RegWrite = 1'b1;
+                                          MemtoReg = 1'b0;
+                                          RegDst = 1'b0;
+                                          opcplus4 = 32'h00000018; 
+               end
+        #20 $finish;
+    end 
+    always #5 clock = ~clock;            
+endmodule
--- src/project_10/instruction_decoder/instruction_decoder.srcs/sources_1/new/decoder.v
+++ src/project_10/instruction_decoder/instruction_decoder.srcs/sources_1/new/decoder.v
@@ -19,7 +19,7 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
+//`default_nettype wire
 module decoder(
     input[31:0] Instruction,    // 取指单元获得的指令
     input[31:0] read_data,      // 来自 DATA RAM or I/O port 的 数据
@@ -33,7 +33,8 @@ module decoder(
     input rst,                  // 复位信号
     output wire [31:0] read_data_1,   // 输出的第一位操作数
     output wire [31:0] read_data_2,   // 输出的第二位操作数
-    output[31:0] Sign_extend    // 译码单元输出的拓展后的32位立即数 
+    output [31:0] Sign_extend,   // 译码单元输出的拓展后的32位立即数
+    input [31:0] register[0:31]    // 寄存器文件共32个32位寄存器
     );
     reg [31:0] register[0:31];              // 寄存器文件共32个32位寄存器
     reg [4:0] write_register_address;       // 需要写入的寄存器地址
@@ -43,48 +44,80 @@ module decoder(
     wire[4:0] write_register_1_address;     // R-type 指令需要写入的寄存器（rd）的地址
     wire[4:0] write_register_2_address;     // I-type 指令需要写入的寄存器（rt）的地址
     wire[15:0] Instruction_immediate_value; // 指令中的立即数
-    wire[5:0] opcode;                      // 指令码
+    wire[5:0] opcode;                       // 指令码
     wire[5:0] funct;
+    reg R_type, I_type, J_type, C_type;    // C_type All coprocessor instructions
+    reg andi, ori;
+
     /* 指令中各分量的提取 */
     assign opcode = Instruction[31:26];                     // OP
-    assign funct = Instruction[5:0];                        // OP
+    assign funct = Instruction[5:0];                        // funct
+    always@* begin
+        {andi, ori} = 2'b00;
+        if (I_type)
+            case(opcode)
+            6'b001100: begin {andi, ori} = 2'b10; $display("andi"); end
+            6'b001101: begin {andi, ori} = 2'b01; $display("ori"); end
+            endcase
+    end
+    
     assign read_register_1_address = Instruction[25:21];    // rs
     assign read_register_2_address = Instruction[20:16];    // rt (R-type)
     assign write_register_1_address = Instruction[15:11];   // rd (R-type)
     assign write_register_2_address = Instruction[20:16];   // rt (I-type)
     assign Instruction_immediate_value = Instruction[15:0]; // data, rladr (I-type)
-    /* 读取寄存器 */ // 不确定
-    assign read_data_1 = read_data;
-    assign read_data_2 = read_data;
-    /* 目标寄存器的指定 */ 
-    always @* begin
-        if (opcode == 6'b0000_00)   // R-type
-            if (write_register_1_address != 5'b0_0000)  // 避免写入 0 号寄存器
+    
+    /* 读取寄存器 */
+    assign read_data_1 = register[read_register_1_address];
+//    assign read_data_2 = (R_type) ? register[read_register_2_address] : Instruction_immediate_value;
+    assign read_data_2 = register[read_register_2_address];
+    
+    
+    always @* begin 
+        {R_type, I_type, J_type, C_type} = 4'b0000;
+            case(opcode)
+            6'b0000_00: {R_type, I_type, J_type, C_type} = 4'b1000;
+            6'b0000_1x: {R_type, I_type, J_type, C_type} = 4'b0010;
+            6'b0100_xx: {R_type, I_type, J_type, C_type} = 4'b0001;
+            default:    {R_type, I_type, J_type, C_type} = 4'b0100;
+            endcase
+            $display("time:%3dns, {R_type, I_type, J_type, C_type} = %b", $time, {R_type, I_type, J_type, C_type});
+        /* 目标寄存器的指定 */ 
+        if (R_type)     // R-type => rd
+            if (write_register_1_address != 5'b0_0000) begin  // 避免写入 0 号寄存器
                 write_register_address = write_register_1_address;
-        else if (opcode != 6'b0100xx) // I-type
-            if (write_register_2_address != 5'b0_0000)  // 避免写入 0 号寄存器
+                $display("time:%3dns, R_type, write_register_address = %d, write_register_1_address = %d, Instruction = %b", $time, write_register_address, write_register_1_address, Instruction);
+            end
+        if (I_type) begin// I-type => rt
+            write_register_address = 5'b0_0001;
+            if (write_register_2_address != 5'b0_0000) begin // 避免写入 0 号寄存器
                 write_register_address = write_register_2_address;
-        else if (Jal)    // Jal
+                $display("time:%3dns, I_type, write_register_address = %d, write_register_2_address = %d, Instruction = %b", $time, write_register_address, write_register_2_address, Instruction);
+            end
+        end
+        if (Jal)   // Jal
             write_register_address = 5'd31;
+        $display ("time = %3d, write_register_address = %d, write_register_1_address = %d, write_register_2_address = %d", $time, write_register_address, write_register_1_address, write_register_2_address);
+        /* 准备要写的数据 */
+        if (~MemtoReg) write_date = ALU_result;
+        else write_date = read_data;
+        if (RegDst) write_date = opcplus4;
+        $monitor ("time:%3dns, write_date = %h\n", $time, write_date);
     end
-    /* 准备要写的数据 */
-    always@* begin
-        if (~MemtoReg)
-            write_date = ALU_result;
-        else if (Jal)
-            write_date = opcplus4;
-    end
+    
     /* 对寄存器的写入操作 */
     integer i;
     always @(posedge clk) begin
         if (rst == 1)
             for (i = 0; i < 32; i = i + 1)
-                register[i] <= 0;
-        else if (RegWrite == 1) begin
-            register[write_register_address] <= write_date;
+                register[i] <= i;
+        else begin
+            if (RegWrite == 1) register[write_register_address] <= write_date;
         end
     end
     /* 立即数的扩展 */
-    
+    wire sign;
+    assign sign = ~(andi || ori) && Instruction_immediate_value[15]; // andi ori: ZeroExtImm else SignExtImm
+    assign Sign_extend = sign ? {16'hffff, Instruction_immediate_value}:{16'h0000, Instruction_immediate_value};
     
 endmodule
